b0b56be194afc7de7d850dd5c39c0e3968fb222a
JeromeMberia <jeromemberia@gmail.com>
2020-05-25 08:08:48 +0300

    adding routes

--- app_gallery/urls.py
+++ app_gallery/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 
 urlpatterns=[
-    path('',views.welcome,name = 'welcome'),
+    path('',views.gallery,name = 'gallery'),
+    path('location/<location_name>',views.location,name ='location'),
+    path('search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
