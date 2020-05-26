a6b177acdc6aafa4a95de1b1a6f82a318aa10c01
JeromeMberia <jeromemberia@gmail.com>
2020-05-26 04:50:34 +0300

    Creating Location model

--- app_gallery/models.py
+++ app_gallery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    pic_location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.pic_location
+
+    
+
+    @classmethod
+    def get_location(cls, id):
+        the_location = Location.objects.get(id=id)
+        return the_location
+
+
+  
+   
+    
+    
+    
+
+
